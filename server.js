1d84086ed2cea1f7967814f337a988b93e6610cc
Levipasha <abbupasha61@gmail.com>
2026-05-09 16:54:55 +0530

    Fix CORS configuration for all deployed applications

--- server.js
+++ server.js
@@ -106,8 +106,7 @@ const corsOptions = {
       'http://localhost:5174',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:5173',
-      'http://127.0.0.1:5174',
-      'https://artiest-dashbaord.vercel.app'
+      'http://127.0.0.1:5174'
     ];
     
     if (allowedOrigins.includes(origin)) {
